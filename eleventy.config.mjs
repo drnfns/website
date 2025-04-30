9e658650f2b5d2c58ec2a2bd5148b6fb859cbbfc
drnfns <work@catsonthe.cloud>
2025-04-30 16:23:47 -0400

    bypass 11ty css copy

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,6 +1,5 @@
 export default function (eleventyConfig) {
   eleventyConfig.setInputDirectory("site");
-  eleventyConfig.addPassthroughCopy("site/output.css");
   eleventyConfig.addPassthroughCopy("site/theme.js");
   eleventyConfig.addPassthroughCopy("site/assets");
   eleventyConfig.setIncludesDirectory("includes");
