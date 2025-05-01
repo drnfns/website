8ba77f60fd1c7459569c2d865a250823df38e9ed
drnfns <work@catsonthe.cloud>
2025-05-01 14:48:05 -0400

    copy robots.txt

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -2,6 +2,7 @@ export default function (eleventyConfig) {
   eleventyConfig.setInputDirectory("site");
   eleventyConfig.addPassthroughCopy("site/theme.js");
   eleventyConfig.addPassthroughCopy("site/assets");
+  eleventyConfig.addPassthroughCopy("site/robots.txt");
   eleventyConfig.setIncludesDirectory("includes");
 };
 
